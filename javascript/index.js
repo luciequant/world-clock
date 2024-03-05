72dcadc3313c44e8b1435d6f42edbf341750662c
luciequant <156855912+luciequant@users.noreply.github.com>
2024-03-05 12:12:53 +0100

    Added current date and time of a location

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,29 @@
+function updateTime() {
+  //Vancouver
+  let vancouverElement = document.querySelector("#vancouver");
+  if (vancouverElement) {
+    let vancouverDataElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTime = moment().tz("America/Vancouver");
 
+    vancouverDataElement.innerHTML = vancouverTime.format("MMMM Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTime.format(
+      "h:mm:ss [<small>]A[<small>]"
+    );
+  }
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDataElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDataElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[<small>]"
+    );
+  }
+}
+
+updateTime();
+setInterval(updateTime, 1000);
