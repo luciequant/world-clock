0db7ae3f65cbc9b058d99bcc7c788d92c3d8536a
luciequant <156855912+luciequant@users.noreply.github.com>
2024-03-05 13:56:29 +0100

    Added time interval to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -31,19 +31,21 @@ function updateCity(event) {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  setInterval(() => {
+    let cityTime = moment().tz(cityTimeZone);
+    citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
+      "A"
+    )}</small></div>
   </div>
   <a href="/" class="go-back">Go back</a>`;
+  }, 1000);
 }
 
 updateTime();
